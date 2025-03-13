983aa55046f1ce48db30989187e3a03af406e464
Rifqi Akhmad Maulana <rifki.ahmad2003@gmail.com>
2025-03-13 17:01:48 +0700

    feat: added executionTime response

--- fibonacci-service/index.js
+++ fibonacci-service/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { performance } from 'perf_hooks'
 
 const app = express();
 
@@ -10,8 +11,24 @@ function fibonacci(n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+app.get('', (_req, res) => {
+    const start = performance.now();
+    const result = fibonacci(30);
+    const end = performance.now();
+    res.json({ 
+        result, 
+        executionTime: `${(end - start).toFixed(6)} ms` 
+    });
+})
+
 app.get('/:n', (req, res) => {
-    res.json({ result: fibonacci(parseInt(req.params.n)) });
+    const start = performance.now();
+    const result = fibonacci(parseInt(req.params.n));
+    const end = performance.now();
+    res.json({ 
+        result,
+        executionTime: `${(end - start).toFixed(6)} ms`  
+    });
 })
 
 const port = process.env.PORT || 3000;
